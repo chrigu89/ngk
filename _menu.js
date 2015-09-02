646afc93b6e7ce95f788de1107aae9abd98d4ed6
chrigu89 <cb@design-busse.de>
2015-09-02 17:31:48 +0200

    Update

--- _menu.js
+++ _menu.js
@@ -5,13 +5,14 @@ var id_a=new Array();
 id_a[activ]=' id="active"';
 
 menu='<ul>'
-+'<li><a'+id_a[0]+' rel="index.php" href="index.html" ><i class="fa fa-home"></i>Home</a></li>'
-+'<li><a'+id_a[1]+' rel="termine.php"  href="termine.html"><i class="fa fa-calendar"></i>Termine</a></li>'
-+'<li><a'+id_a[2]+' rel="wissen.php"  href="wissen.html"><i class="fa fa-graduation-cap "></i>Wissen</a></li>'
-+'<li><a'+id_a[3]+' rel="jahrbuch.html" href="jahrbuch.html" ><i class="fa fa-book"></i>Jahrbuch</a></li>'
-+'<li><a'+id_a[4]+' rel="korpsschiessen.html" href="korpsschiessen.html" ><i class="fa fa-tasks"></i>Korpsschiessen</a></li>'
-+'<li><a'+id_a[5]+' rel="vorstand.html" href="vorstand.html" ><i class="fa fa-male"></i>Vorstand</a></li>'
-+'<li><a'+id_a[6]+' rel="achterausschuss.html" href="achterausschuss.html" ><i class="fa fa-male"></i>Achterausschuss</a></li>'
-+'<li><a'+id_a[7]+' rel="settings.html" href="settings.html" ><i class="fa fa-cog"></i>Einstellungen</a></li>'
++'<li><a'+id_a[0]+' href="index.html" ><i class="fa fa-home"></i>Home</a></li>'
++'<li><a'+id_a[1]+' href="neuigkeiten.html"><i class="fa fa-calendar"></i>Neuigkeiten</a></li>'
++'<li><a'+id_a[2]+' href="termine.html"><i class="fa fa-calendar"></i>Termine</a></li>'
++'<li><a'+id_a[3]+' href="wissen.html"><i class="fa fa-graduation-cap "></i>Wissen</a></li>'
++'<li><a'+id_a[4]+' href="jahrbuch.html" ><i class="fa fa-book"></i>Jahrbuch</a></li>'
++'<li><a'+id_a[5]+' href="korpsschiessen.html" ><i class="fa fa-tasks"></i>Korpsschiessen</a></li>'
++'<li><a'+id_a[6]+' href="vorstand.html" ><i class="fa fa-male"></i>Vorstand</a></li>'
++'<li><a'+id_a[7]+' href="achterausschuss.html" ><i class="fa fa-male"></i>Achterausschuss</a></li>'
++'<li><a'+id_a[8]+' href="settings.html" ><i class="fa fa-cog"></i>Einstellungen</a></li>'
 +'</ul>';
 document.write(menu);
