111b4c1dc8bff2247849719b65a3bf14e1e7c65f
chrigu89 <cb@design-busse.de>
2017-09-12 12:16:12 +0200

    asd

--- js/scripts.js
+++ js/scripts.js
@@ -160,6 +160,106 @@ var onReady = function() {
 	}
 	
 	
+
+
+		var calenderId = 1;
+		var calenderName  ="";
+
+		var successLoad = function(message) {
+
+			var calender = message[0];
+			calenderId = calender["id"];
+			calenderName = calender["name"];
+
+		};
+		var errorLoad = function(message) {
+			//alert("Sorry, es ist ein Fehler aufgetreten. Bitte wenden Sie sich an Christian Busse <christian.busse@apfel.gold>");
+			//return false;
+		};
+
+		window.plugins.calendar.listCalendars(successLoad,errorLoad);
+
+		
+
+		function allEvents(dat) {
+
+			var success = function(message) {
+				//alertObject(message);
+
+			};
+
+			var error = function(message) {
+				alert("Sorry, es ist ein Fehler aufgetreten. Bitte wenden Sie sich an Christian Busse <christian.busse@apfel.gold>");
+				//return false;
+			};
+
+			var doAction = confirm('Möchtest du alle Termine in deinen Kalender "'+calenderName+'" eintragen? Um Dublikate zu vermeiden werden alle Termine mit gleichem Namen vor dem Eintragen gelöscht.');
+
+
+			if(doAction == false) {
+
+			} else {
+
+
+				$("#load_").fadeIn(300, 'easeInQuart', function() {
+					$('#load_').addClass("loader_img");
+
+					setTimeout(function(){
+
+					for (var i = 0; i < termine_array.length; ++i){
+						var heute_date = new Date();	
+
+						var title = termine_array[i]["title"];
+						var location = termine_array[i]["location"];
+						var notes = termine_array[i]["teaser"];
+
+						tmp_array1 = new Array();
+						tmp_array2 = new Array();
+						tmp_array3 = new Array();
+
+						tmp_array1 = termine_array[i]["datum"].split('.');
+						str_=tmp_array1.join(':');
+						tmp_array2=str_.split(':');
+						str_=tmp_array2.join(' ');
+						tmp_array3=str_.split(' ');
+
+						var calOptions = window.plugins.calendar.getCalendarOptions();
+						//calOptions.calendarId = calenderId;
+						//calOptions.calendarId = 6;
+
+						var startDate = new Date(tmp_array3[2], (tmp_array3[1] - 1), tmp_array3[0], tmp_array3[3], tmp_array3[4], 0, 0, 0);
+						var endDate = new Date(tmp_array3[2], (tmp_array3[1] - 1), tmp_array3[0], tmp_array3[3], (tmp_array3[4] + 1), 0, 0, 0);
+
+						if(startDate > heute_date){		
+							window.plugins.calendar.deleteEvent(title, location, notes, startDate, endDate, success, error);
+							sleep(150);
+							window.plugins.calendar.createEvent(title, location, notes, startDate, endDate, success, error);
+							sleep(150);
+
+						}
+
+
+					}
+
+					alert('Alle Termine wurden erfolgreich im Kalender "'+calenderName+'" eingetragen.');
+
+
+					$("#load_").fadeOut(300, 'easeInQuart', function() {
+						$('#load_').removeClass("loader_img");
+					});
+
+
+					}, 300);
+
+				});
+
+			}
+			return false;
+
+
+		}
+
+
 	
     function neu_seite(url) {
 
