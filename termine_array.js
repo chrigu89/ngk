92684363627dee1c47df99ea8de86cca83297d80
chrigu89 <cb@design-busse.de>
2018-08-30 20:04:52 +0200

    Update Countdown und Termine

--- termine_array.js
+++ termine_array.js
@@ -2,90 +2,6 @@ termine_array = new Array();
 
 termine_array = [
 		{
-			'datum'			: "20.03.2018 20:00",
-			'title'			: 'Fackelbaubesprechung',
-			'teaser'		: "",
-			'location'		: "Marienhaus, Kapitelstraße 36",
-		},
-		{
-			'datum'			: "14.04.2018 13:00",
-			'title'			: 'Korpsschießen',
-			'teaser'		: "",
-			'location'		: "Scheibenstand, Scheibendamm",
-		},
-		{
-			'datum'			: "15.05.2018 20:00",
-			'title'			: 'Chargiertenversammlung',
-			'teaser'		: "",
-			'location'		: "Marienhaus, Kapitelstraße 36",
-		},
-		{ 	
-			'datum'			: "24.06.2018 10:00",
-			'title'			: 'Patronatstag',
-			'teaser'		: "Heilige Messe - St. Marien Umzug zum Foyer Rheinisches Landestheater musikalischer Frühschoppen Siegerehrung Korpsschießen, Ehrungen, Zuganmeldung Schützenfest 2018",
-			'location'		: "St. Marien",
-		},
-		{
-			'datum'			: "14.07.2018 19:00",
-			'title'			: 'Bürgerversammlung',
-			'teaser'		: "",
-			'location'		: "Stadthalle Neuss",
-		},
-		{
-			'datum'			: "28.07.2018 19:00",
-			'title'			: 'Majorsehrenabend',
-			'teaser'		: "Stadthalle Neuss, Große Korpsversammlung Jubilarehrung, anschl. Heimgeleit des Majors und Biwak am Grenadier-Schützen-Denkmal ",
-			'location'		: "Stadthalle Neuss",
-		},
-		{
-			'datum'			: "30.07.2018 19:00",
-			'title'			: 'Marschübung (bei Bedarf)',
-			'teaser'		: "",
-			'location'		: "Parkplatz Rennbahn / Globe Theater",
-		},
-		{
-			'datum'			: "04.08.2018 18:30",
-			'title'			: 'Oberstehrenabend',
-			'teaser'		: "",
-			'location'		: "Stadthalle Neuss",
-		},
-		{
-			'datum'			: "06.08.2018 20:00",
-			'title'			: 'Zugführerbesprechung',
-			'teaser'		: "",
-			'location'		: "Marienhaus, Kapitelstaße 36",
-		},
-		{
-			'datum'			: "11.08.2018 17:30",
-			'title'			: 'Königsehrenabend',
-			'teaser'		: "",
-			'location'		: "Stadthalle Neuss",
-		},
-		{
-			'datum'			: "19.08.2018 11:00",
-			'title'			: 'Promenadenkonzert des NGK',
-			'teaser'		: "",
-			'location'		: "Platz am Weißen Haus",
-		},
-		{
-			'datum'			: "20.08.2018 17:30",
-			'title'			: 'Fackelbesichtigung',
-			'teaser'		: "",
-			'location'		: "Fackelbauhalle Zietzschmannhalle",
-		},
-		{
-			'datum'			: "24.08.2018 17:00",
-			'title'			: 'Kirmesplatz Eröffnung',
-			'teaser'		: "",
-			'location'		: "Hessentorbrücke",
-		},
-		{
-			'datum'			: "25.08.2018 12:00 - 28.08.2018 23:59",
-			'title'			: "Neusser Bürger-Schützenfest",
-			'teaser'		: "25.08.2018 - 28.08.2018",
-			'location'		: "",
-		},
-		{
 			'datum'			: "01.09.2018 18:00 - 01.09.2018 20:00",
 			'title'			: 'Krönungszug / Krönungsball',
 			'teaser'		: "Abmarsch der Chargierten",
@@ -169,8 +85,7 @@ termine_array = [
 			'title'			: 'Marschübung (bei Bedarf)',
 			'teaser'		: "",
 			'location'		: "Parkplatz Rennbahn / Globe Theater",
-		},
-		
+		},		
 		{
 			'datum'			: "03.08.2019 18:30",
 			'title'			: 'Oberstehrenabend',
@@ -210,7 +125,7 @@ termine_array = [
 		{
 			'datum'			: "24.08.2019 12:00 - 27.08.2019 23:59",
 			'title'			: "Neusser Bürger-Schützenfest",
-			'teaser'		: "24.08.2018 - 27.08.2018",
+			'teaser'		: "24.08.2019 - 27.08.2019",
 			'location'		: "",
 		},
 		{
