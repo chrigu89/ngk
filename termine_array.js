103b274e431088b8c54f54793c49c52f5df42314
cb@design-busse.de <DTxhruKb5q>
2019-02-06 08:52:53 +0100

    Update Termine

--- termine_array.js
+++ termine_array.js
@@ -2,42 +2,6 @@ termine_array = new Array();
 
 termine_array = [
 		{
-			'datum'			: "01.09.2018 18:00 - 01.09.2018 20:00",
-			'title'			: 'Krönungszug / Krönungsball',
-			'teaser'		: "Abmarsch der Chargierten",
-			'location'		: "Stadthalle Neuss",
-		},
-		{
-			'datum'			: "11.09.2018 20:00",
-			'title'			: 'Chargiertenversammlung',
-			'teaser'		: "",
-			'location'		: "Marienhaus, Kapitelstraße 36",
-		},
-		{
-			'datum'			: "13.09.2018 00:00 - 19.09.2018 23:59",
-			'title'			: 'Steubenparade New York',
-			'teaser'		: "13.09.2018 - 19.09.2018",
-			'location'		: "",
-		},
-		{
-			'datum'			: "02.10.2018 17:00",
-			'title'			: 'Schützen-Wiesn - Oktoberfest mit BRINGS',
-			'teaser'		: "",
-			'location'		: "Festzelt auf dem Rennbahn Park",
-		},
-		{
-			'datum'			: "18.11.2018 11:30",
-			'title'			: 'Volkstrauertag',
-			'teaser'		: "",
-			'location'		: "Hauptfriedhof, Rheydter Straße",
-		},
-		{
-			'datum'			: "23.11.2018 20:00",
-			'title'			: 'Jahreshauptversammlung',
-			'teaser'		: "Neusser Bürger-Schützen-Verein",
-			'location'		: "Sparkassen – Forum Haus II, Sparkasse Neuss Michaelstraße",
-		},
-		{
 			'datum'			: "01.02.2019 20:00",
 			'title'			: 'NGK-Jahreshauptversammlung',
 			'teaser'		: "",
@@ -159,8 +123,87 @@ termine_array = [
 			'location'		: "Sparkassen – Forum Haus II, Sparkasse Neuss Michaelstraße",
 		},
 		{
-			'datum'			: "31.01.2020 20:00",
+			'datum'			: "07.02.2020 20:00",
 			'title'			: 'NGK-Jahreshauptversammlung',
+			'teaser'		: "",
+			'location'		: "Marienhaus, Kapitelstraße 36",
+		},
+		{
+			'datum'			: "31.03.2020 20:00",
+			'title'			: 'Fackelbaubesprechung',
+			'teaser'		: "",
+			'location'		: "Marienhaus, Kapitelstraße 36",
+		},
+		{
+			'datum'			: "18.04.2020 13:00",
+			'title'			: 'Korpsschießen',
+			'teaser'		: "",
+			'location'		: "Scheibenstand, Scheibendamm",
+		},
+		{
+			'datum'			: "12.05.2020 20:00",
+			'title'			: 'Chargiertenversammlung',
+			'teaser'		: "",
+			'location'		: "Marienhaus, Kapitelstraße 36",
+		},
+		{ 	
+			'datum'			: "28.06.2020 10:00",
+			'title'			: 'Patronatstag',
+			'teaser'		: "Heilige Messe - St. Marien Umzug zum Foyer Rheinisches Landestheater musikalischer Frühschoppen Siegerehrung Korpsschießen, Ehrungen, Zuganmeldung Schützenfest 2018",
+			'location'		: "St. Marien",
+		},
+		
+		{
+			'datum'			: "01.08.2020 19:00",
+			'title'			: 'Majorsehrenabend',
+			'teaser'		: "Stadthalle Neuss, Große Korpsversammlung Jubilarehrung, anschl. Heimgeleit des Majors und Biwak am Grenadier-Schützen-Denkmal ",
+			'location'		: "Stadthalle Neuss",
+		},	
+		{
+			'datum'			: "08.08.2020 18:30",
+			'title'			: 'Oberstehrenabend',
+			'teaser'		: "",
+			'location'		: "Stadthalle Neuss",
+		},
+		{
+			'datum'			: "10.08.2020 20:00",
+			'title'			: 'Zugführerbesprechung',
+			'teaser'		: "",
+			'location'		: "Marienhaus, Kapitelstaße 36",
+		},
+		{
+			'datum'			: "15.08.2020 17:30",
+			'title'			: 'Königsehrenabend',
+			'teaser'		: "",
+			'location'		: "Stadthalle Neuss",
+		},
+		{
+			'datum'			: "23.08.2020 11:00",
+			'title'			: 'Promenadenkonzert des NGK',
+			'teaser'		: "",
+			'location'		: "Platz am Weißen Haus",
+		},		
+		{
+			'datum'			: "28.08.2020 17:00",
+			'title'			: 'Kirmesplatz Eröffnung',
+			'teaser'		: "",
+			'location'		: "Hessentorbrücke",
+		},
+		{
+			'datum'			: "29.08.2020 12:00 - 01.09.2020 23:59",
+			'title'			: "Neusser Bürger-Schützenfest",
+			'teaser'		: "29.08.2020 - 01.09.2020",
+			'location'		: "",
+		},
+		{
+			'datum'			: "05.09.2020 18:00 - 05.09.2020 20:00",
+			'title'			: 'Krönungszug / Krönungsball',
+			'teaser'		: "Abmarsch der Chargierten",
+			'location'		: "Stadthalle Neuss",
+		},
+		{
+			'datum'			: "15.09.2020 20:00",
+			'title'			: 'Chargiertenversammlung',
 			'teaser'		: "",
 			'location'		: "Marienhaus, Kapitelstraße 36",
 		},
