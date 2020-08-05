18ff6774fae959e346aae23a8858aa5258aa31dc
chrigu89 <cb@design-busse.de>
2020-08-05 21:00:05 +0200

    Update Termine

--- termine_array.js
+++ termine_array.js
@@ -19,6 +19,135 @@ termine_array = [
 			'teaser'		: "Neusser Bürger-Schützen-Verein",
 			'location'		: "Sparkassen – Forum Haus II, Sparkasse Neuss Michaelstraße",
 		},
+		
+
+		{
+			'datum'			: "05.02.2021 20:00",
+			'title'			: 'NGK-Jahreshauptversammlung',
+			'teaser'		: "",
+			'location'		: "Marienhaus, Kapitelstraße 36",
+		},
+		{
+			'datum'			: "23.03.2021 20:00",
+			'title'			: 'Fackelbaubesprechung',
+			'teaser'		: "",
+			'location'		: "Marienhaus, Kapitelstraße 36",
+		},
+		{
+			'datum'			: "14.04.2021 13:00",
+			'title'			: 'Korpsschießen',
+			'teaser'		: "",
+			'location'		: "Scheibenstand, Scheibendamm",
+		},
+		
+		
+		
+		{
+			'datum'			: "11.05.2021 20:00",
+			'title'			: 'Chargiertenversammlung',
+			'teaser'		: "",
+			'location'		: "Marienhaus, Kapitelstraße 36",
+		},
+		{ 	
+			'datum'			: "27.06.2021 10:00",
+			'title'			: 'Patronatstag',
+			'teaser'		: "Heilige Messe - St. Marien Umzug zum Foyer Rheinisches Landestheater musikalischer Frühschoppen Siegerehrung Korpsschießen, Ehrungen, Zuganmeldung Schützenfest 2018",
+			'location'		: "St. Marien",
+		},
+		/*
+		{
+			'datum'			: "13.07.2021 19:00",
+			'title'			: 'Bürgerversammlung',
+			'teaser'		: "",
+			'location'		: "Stadthalle Neuss",
+		},	
+		
+		*/
+		{
+			'datum'			: "31.07.2021 19:00",
+			'title'			: 'Majorsehrenabend',
+			'teaser'		: "Stadthalle Neuss, Große Korpsversammlung Jubilarehrung, anschl. Heimgeleit des Majors und Biwak am Grenadier-Schützen-Denkmal ",
+			'location'		: "Stadthalle Neuss",
+		},
+		
+		
+		{
+			'datum'			: "02.08.2021 19:00",
+			'title'			: 'Marschübung (bei Bedarf)',
+			'teaser'		: "",
+			'location'		: "Parkplatz Rennbahn / Globe Theater",
+		},		
+		{
+			'datum'			: "07.08.2021 18:30",
+			'title'			: 'Oberstehrenabend',
+			'teaser'		: "",
+			'location'		: "Stadthalle Neuss",
+		},
+		{
+			'datum'			: "09.08.2021 20:00",
+			'title'			: 'Zugführerbesprechung',
+			'teaser'		: "",
+			'location'		: "Marienhaus, Kapitelstaße 36",
+		},
+		
+		
+		{
+			'datum'			: "14.08.2021 17:30",
+			'title'			: 'Königsehrenabend',
+			'teaser'		: "",
+			'location'		: "Stadthalle Neuss",
+		},
+		{
+			'datum'			: "22.08.2021 11:00",
+			'title'			: 'Promenadenkonzert des NGK',
+			'teaser'		: "",
+			'location'		: "Platz am Weißen Haus",
+		},
+		
+		{
+			'datum'			: "23.08.2021 17:30",
+			'title'			: 'Fackelbesichtigung',
+			'teaser'		: "",
+			'location'		: "Fackelbauhalle Zietzschmannhalle",
+		},
+		{
+			'datum'			: "27.08.2021 17:00",
+			'title'			: 'Kirmesplatz Eröffnung',
+			'teaser'		: "",
+			'location'		: "Hessentorbrücke",
+		},
+		{
+			'datum'			: "28.08.2021 12:00 - 31.08.2021 23:59",
+			'title'			: "Neusser Bürger-Schützenfest",
+			'teaser'		: "28.08.2021 - 31.08.2021",
+			'location'		: "",
+		},
+		{
+			'datum'			: "04.09.2021 18:00",
+			'title'			: 'Krönungszug / Krönungsball',
+			'teaser'		: "Abmarsch der Chargierten",
+			'location'		: "Stadthalle Neuss",
+		},
+		{
+			'datum'			: "14.09.2021 20:00",
+			'title'			: 'Chargiertenversammlung',
+			'teaser'		: "",
+			'location'		: "Marienhaus, Kapitelstraße 36",
+		},
+		{
+			'datum'			: "14.11.2021 11:30",
+			'title'			: 'Volkstrauertag',
+			'teaser'		: "",
+			'location'		: "Hauptfriedhof, Rheydter Straße",
+		},
+		/*
+		{
+			'datum'			: "22.11.2021 20:00",
+			'title'			: 'Jahreshauptversammlung',
+			'teaser'		: "Neusser Bürger-Schützen-Verein",
+			'location'		: "Sparkassen – Forum Haus II, Sparkasse Neuss Michaelstraße",
+		},
+		*/
 
 	];
 
